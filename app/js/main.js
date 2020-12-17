32c61689308f0e04af6aae870187ad5b200998e8
DevMinrat <devminrat@gmail.com>
2020-12-17 20:27:17 +0300

    added gallery section

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,28 @@
-$(function () {});
+$(function () {
+  var fselector = 0;
+
+  var $grid = $(".gallery__items").isotope({
+    itemSelector: ".gallery__item",
+    layoutMode: "fitRows",
+  });
+  // filter functions
+  var filterFns = {};
+  // bind filter button click
+  $(".filters-button-group").on("click", "button", function () {
+    var filterValue = $(this).attr("data-filter");
+    // use filterFn if matches value
+    filterValue = filterFns[filterValue] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+
+  $(".button-group").each(function (i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on("click", "button", function () {
+      $buttonGroup.find(".is-checked").removeClass("is-checked");
+      $(this).addClass("is-checked");
+    });
+  });
+
+  // filter .men items and hide other
+  $grid.isotope({ filter: ".men" });
+});
